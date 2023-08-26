e34b264d4a62ef5642d96740752e29710543cce7
fafa-a <fabienpuech1@gmail.com>
2023-08-26 21:09:23 +0200

    feat(ut): connected steps

--- src/test/posting-message.spec.ts
+++ src/test/posting-message.spec.ts
@@ -1,22 +1,46 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from 'vitest';
 
-describe("Feature: Posting a message", () => {
-  describe("Rule: A message can contain a maximum of 280 characters", () => {
-    test("Alice can post a message on her timeline", async () => {
-      givenNowIs(new Date("2023-01-19T19:00:00.000Z"))
-      whenUserPostsAMessage({ id: "message-id", text: "Hello World", author: "Alice" })
-      thenPostedMessageShouldBe({ id: "message-id", text: "Hello World", author: "Alice", publishedAt: new Date("2023-01-19T19:00:00.000Z")
-      })
+describe('Feature: Posting a message', () => {
+  describe('Rule: A message can contain a maximum of 280 characters', () => {
+    test('Alice can post a message on her timeline', async () => {
+      givenNowIs(new Date('2023-01-19T19:00:00.000Z'));
+      whenUserPostsAMessage({
+        id: 'message-id',
+        text: 'Hello World',
+        author: 'Alice',
+      });
+      thenPostedMessageShouldBe({
+        id: 'message-id',
+        text: 'Hello World',
+        author: 'Alice',
+        publishedAt: new Date('2023-01-19T19:00:00.000Z'),
+      });
     });
   });
 });
 
-function givenNowIs(now: Date) {
+let message: { id: string; text: string; author: string; publishedAt: Date };
+
+let now: Date;
+
+function givenNowIs(_now: Date) {
+  now = _now;
 }
 
-function whenUserPostsAMessage(postMessageCommand: { id: string; text: string; author: string; }) {
+function whenUserPostsAMessage(postMessageCommand: { id: string; text: string; author: string }) {
+  message = {
+    id: postMessageCommand.id,
+    text: postMessageCommand.text,
+    author: postMessageCommand.author,
+    publishedAt: now,
+  };
 }
 
-function thenPostedMessageShouldBe(expectedMessage: { id: string; text: string; author: string; publishedAt: Date; }) {
+function thenPostedMessageShouldBe(expectedMessage: {
+  id: string;
+  text: string;
+  author: string;
+  publishedAt: Date;
+}) {
+  expect(expectedMessage).toEqual(message);
 }
-
