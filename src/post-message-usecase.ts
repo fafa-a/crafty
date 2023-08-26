2515f361730fc89c823d948718e17d25cbe2788f
fafa-a <fabienpuech1@gmail.com>
2023-08-26 22:19:50 +0200

    feat(ut): use case & inversion of dependency

---
+++ src/post-message-usecase.ts
@@ -0,0 +1,32 @@
+export type Message = {
+  id: string;
+  text: string;
+  author: string;
+  publishedAt: Date;
+};
+
+export type PostMessageCommand = Omit<Message, 'publishedAt'>;
+
+export interface MessageRepository {
+  save(msg: Message): void;
+}
+
+export interface DateProvider {
+  getNow(): Date;
+}
+
+export class PostMessageUseCase {
+  constructor(
+    private readonly messageRepository: MessageRepository,
+    private readonly dateProvider: DateProvider,
+  ) {}
+
+  handle(postMessageCommand: PostMessageCommand) {
+    this.messageRepository.save({
+      id: postMessageCommand.id,
+      text: postMessageCommand.text,
+      author: postMessageCommand.author,
+      publishedAt: this.dateProvider.getNow(),
+    });
+  }
+}
--- src/test/posting-message.spec.ts
+++ src/test/posting-message.spec.ts
@@ -1,14 +1,23 @@
 import { describe, expect, test } from 'vitest';
+import {
+  DateProvider,
+  Message,
+  MessageRepository,
+  PostMessageCommand,
+  PostMessageUseCase,
+} from '../post-message-usecase';
 
 describe('Feature: Posting a message', () => {
   describe('Rule: A message can contain a maximum of 280 characters', () => {
     test('Alice can post a message on her timeline', async () => {
       givenNowIs(new Date('2023-01-19T19:00:00.000Z'));
+
       whenUserPostsAMessage({
         id: 'message-id',
         text: 'Hello World',
         author: 'Alice',
       });
+
       thenPostedMessageShouldBe({
         id: 'message-id',
         text: 'Hello World',
@@ -19,28 +28,34 @@ describe('Feature: Posting a message', () => {
   });
 });
 
-let message: { id: string; text: string; author: string; publishedAt: Date };
+let message: Message;
 
-let now: Date;
+class InMemoryMessageRepository implements MessageRepository {
+  save(msg: Message): void {
+    message = msg;
+  }
+}
+
+class StubDateProvider implements DateProvider {
+  now!: Date;
+  getNow(): Date {
+    return this.now;
+  }
+}
+
+const messageRepository = new InMemoryMessageRepository();
+const dateProvider = new StubDateProvider();
+
+const postMessageUseCase = new PostMessageUseCase(messageRepository, dateProvider);
 
 function givenNowIs(_now: Date) {
-  now = _now;
+  dateProvider.now = _now;
 }
 
-function whenUserPostsAMessage(postMessageCommand: { id: string; text: string; author: string }) {
-  message = {
-    id: postMessageCommand.id,
-    text: postMessageCommand.text,
-    author: postMessageCommand.author,
-    publishedAt: now,
-  };
+function whenUserPostsAMessage(postMessageCommand: PostMessageCommand) {
+  postMessageUseCase.handle(postMessageCommand);
 }
 
-function thenPostedMessageShouldBe(expectedMessage: {
-  id: string;
-  text: string;
-  author: string;
-  publishedAt: Date;
-}) {
+function thenPostedMessageShouldBe(expectedMessage: Message) {
   expect(expectedMessage).toEqual(message);
 }
